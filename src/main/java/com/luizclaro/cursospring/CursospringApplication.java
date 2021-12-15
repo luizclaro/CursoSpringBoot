76eaabfcf87d442aff93ba214a640b70b5999b89
luizclaro <luizgustavoclaro@gmail.com>
2021-12-15 16:25:55 -0300

    criando operaçao de instacia

--- src/main/java/com/luizclaro/cursospring/CursospringApplication.java
+++ src/main/java/com/luizclaro/cursospring/CursospringApplication.java
@@ -1,13 +1,30 @@
 package com.luizclaro.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.luizclaro.cursospring.domain.Categoria;
+import com.luizclaro.cursospring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 	}
 
 }
